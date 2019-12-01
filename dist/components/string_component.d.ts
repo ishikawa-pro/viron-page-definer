544d316a6ef5be16c6e3baf088a01075acf04d71
ishikawa-pro <ishikawa.pro@gmail.com>
2019-12-01 23:00:56 +0900

    Removal:unnecessary dist files 🗑

--- dist/components/string_component.d.ts
+++
@@ -1,9 +0,0 @@
-import { IComponent } from './base_component';
-export interface StringComponent extends IComponent {
-    style: 'string';
-    description?: string;
-    required?: boolean | false;
-    maxLength?: number;
-    minLength?: number;
-    patter?: string;
-}
