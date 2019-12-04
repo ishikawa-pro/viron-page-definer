06f83d7598aa915076478d924a4c912b5ec1f5c8
ishikawa-pro <ishikawa.pro@gmail.com>
2019-12-05 00:00:25 +0900

    Add: table_component test  🎉

---
+++ test/table_component.ts
@@ -0,0 +1,27 @@
+import assert from 'assert';
+import { Method } from '../src/api';
+import { TableComponent } from '../src/components';
+
+describe('table_component', () => {
+  it('create table_component class', () => {
+    const tableComponent = new TableComponent(
+      {method: Method.Get, path: '/test'},
+      'test',
+      10,
+      ['/test-action1', '/test-action2'],
+      true,
+      'id',
+      ['id', 'name']
+    );
+    assert.strictEqual(tableComponent.api.method, 'get');
+    assert.strictEqual(tableComponent.api.path, '/test');
+    assert.strictEqual(tableComponent.name, 'test');
+    assert.strictEqual(tableComponent.auto_refresh_sec, 10);
+    assert.strictEqual(tableComponent.actions?.[0], '/test-action1');
+    assert.strictEqual(tableComponent.actions?.[1], '/test-action2');
+    assert.strictEqual(tableComponent.pagination, true);
+    assert.strictEqual(tableComponent.primary, 'id');
+    assert.strictEqual(tableComponent.table_labels?.[0], 'id');
+    assert.strictEqual(tableComponent.table_labels?.[1], 'name');
+  });
+});
