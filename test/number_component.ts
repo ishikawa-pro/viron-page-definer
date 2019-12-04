12bff2e12d019f4058a78cc564e05ee2d3e57fe0
ishikawa-pro <ishikawa.pro@gmail.com>
2019-12-04 22:37:22 +0900

    Add: number_component test  🎉

---
+++ test/number_component.ts
@@ -0,0 +1,19 @@
+import assert from 'assert';
+import * as api from '../src/api';
+import { NumberComponent } from '../src/components';
+
+describe('number_component', () => {
+  it('create NumberComponent class', () => {
+    const numberComponent = new NumberComponent(
+      api.createAPI(api.Method.Get, '/test'),
+      'test',
+      10,
+      'unit'
+    );
+    assert.strictEqual(numberComponent.api.method, 'get');
+    assert.strictEqual(numberComponent.api.path, '/test');
+    assert.strictEqual(numberComponent.name, 'test');
+    assert.strictEqual(numberComponent.auto_refresh_sec, 10);
+    assert.strictEqual(numberComponent.unit, 'unit');
+  });
+});
