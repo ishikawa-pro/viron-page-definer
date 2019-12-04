18ef170fc16b1ca3ce8e840dd52324291681963d
ishikawa-pro <ishikawa.pro@gmail.com>
2019-12-05 00:06:20 +0900

    Refactoring:test directory ♻

--- test/components/table_component.ts
+++ test/components/table_component.ts
@@ -1,6 +1,6 @@
 import assert from 'assert';
-import { Method } from '../src/api';
-import { TableComponent } from '../src/components';
+import { Method } from '../../src/api';
+import { TableComponent } from '../../src/components';
 
 describe('table_component', () => {
   it('create table_component class', () => {
