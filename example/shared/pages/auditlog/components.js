9ce6d9091f31086c0573ecbd6878a15af029b4cf
ishikawa-pro <ishikawa.pro@gmail.com>
2019-12-10 00:31:08 +0900

    Update:example ✨

--- example/shared/pages/auditlog/components.js
+++ example/shared/pages/auditlog/components.js
@@ -2,6 +2,7 @@ const {
   createAPI,
   Method,
   TableComponent,
+  SearchQueryType,
 } = require('viron-page-definer');
 
 module.exports = [
@@ -10,6 +11,14 @@ module.exports = [
     'Viron 監査ログ',
     null,
     null,
+    [
+      {key: 'createdAt', type: SearchQueryType.string},
+      {key: 'request_uri', type: SearchQueryType.string},
+      {key: 'request_method', type: SearchQueryType.string},
+      {key: 'source_ip', type: SearchQueryType.string},
+      {key: 'status_code', type: SearchQueryType.integer},
+      {key: 'user_id', type: SearchQueryType.string},
+    ],
     'role_id',
     ['createdAt', 'request_uri', 'request_method', 'status_code', 'user_id']
   )
